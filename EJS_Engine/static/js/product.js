3c1265dcbad2d0d3c9e3331265f7f4fa5a3e42b1
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-03-23 22:32:20 +0500

    created the product page using ejs engine

---
+++ EJS_Engine/static/js/product.js
@@ -0,0 +1,8 @@
+function nav_open(){
+    document.getElementById('nav-side').style.display='block';
+    document.getElementById('op').style.opacity='0.4';
+}
+function nav_close(){
+    document.getElementById('nav-side').style.display='none';
+    document.getElementById('op').style.opacity='1';
+}
